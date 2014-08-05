f3703a999e23d245e2ed1c2f11154475cde4fb51
Chris Cioce <ccioce@mail.usf.edu>
2014-08-05 12:07:31 -0700

    added num_threads

--- hello_hybrid.c
+++ hello_hybrid.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
            iam, np, rank, numprocs, processor_name);
   }
 
-  #pragma omp parallel default(shared) private(iam, np)
+  #pragma omp parallel num_threads(3) default(shared) private(iam, np)
   {
     np = omp_get_num_threads();
     iam = omp_get_thread_num();
